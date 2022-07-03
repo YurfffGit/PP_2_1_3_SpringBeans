363d22a77e147e2f13d1017ae92d1189362996da
Yurfff <sidash2002@gmail.com>
2022-07-03 04:18:54 +0300

    Добавлены бины, изменён sout

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,27 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
-
+    Timer timer = new Timer();
+    public Timer getTimer() {
+        return timer;
+    }
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+//        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime()); // Чтобы выполнить условие задачи 4
         System.out.println("________________________");
     }
 }
